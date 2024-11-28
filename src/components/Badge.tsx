966d07df72477871cfc303f559e150176ce0bfa2
ceciliomichael <mtcecilio@student.apc.edu.ph>
2024-11-29 02:11:47 +0800

    yes

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,17 +48,17 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered by'}
+          {props.footer?.text ?? ''}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? ''}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>{props.footer?.company ?? ''}</span>
           </a>
         </span>
       </Show>
